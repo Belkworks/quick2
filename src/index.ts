3a148452f0c3b4b31c8ee0fffb6c50e59eab93ff
safazi <safazi@airmail.cc>
2023-01-21 15:16:58 -0600

    work

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-/**
- * @internal
- */
+/** @internal */
 export class Chain<T> {
 	constructor(protected readonly value: T) {}
 
@@ -12,6 +10,11 @@ export class Chain<T> {
 		return new Chain(thru(this.value));
 	}
 
+	tap(thru: (value: T) => void) {
+		thru(this.value);
+		return this;
+	}
+
 	/**
 	 * Attempt to turn the value into a {@link NumberChain}.
 	 */
@@ -20,6 +23,9 @@ export class Chain<T> {
 		return new NumberChain(this.value);
 	}
 
+	/**
+	 * Attempt to turn the value into a {@link BoolChain}.
+	 */
 	bool() {
 		assert(typeIs(this.value, "boolean"), "Expected boolean");
 		return new BoolChain(this.value);
@@ -42,13 +48,20 @@ export class Chain<T> {
 	}
 }
 
+/** Create a new chain
+ */
 export function chain<T>(value: T) {
 	return new Chain(value);
 }
 
-/**
- * @internal
+/** Turn a chain or normal value into a normal value.
  */
+export function unwrap<T>(value: T | Chain<T>) {
+	if (value instanceof Chain) return value.done();
+	return value;
+}
+
+/** @internal */
 export class BoolChain extends Chain<boolean> {
 	flip() {
 		return new BoolChain(!this.value);
@@ -71,9 +84,7 @@ export function bool(value: boolean) {
 	return new BoolChain(value);
 }
 
-/**
- * @internal
- */
+/** @internal */
 export class NumberChain extends Chain<number> {
 	multiply(other: number) {
 		return new NumberChain(this.value * other);
@@ -130,6 +141,10 @@ export class NumberChain extends Chain<number> {
 	lte(other: number) {
 		return new BoolChain(this.value <= other);
 	}
+
+	eq(other: number) {
+		return new BoolChain(this.value === other);
+	}
 }
 
 export function number(value: number) {
@@ -139,15 +154,15 @@ export function number(value: number) {
 export type Predicate<T> = (item: T, index: number, array: readonly T[]) => boolean;
 export type Transformer<T, U> = (item: T, index: number, array: readonly T[]) => U;
 
-/**
- * @internal
- */
+/** @internal */
 export class ArrayChain<T extends defined> extends Chain<T[]> {
-	/**
-	 * Get the size of the array.
-	 */
+	/** Get the size of the array. */
 	size() {
 		return new NumberChain(this.value.size());
+	}
+
+	isEmpty() {
+		return this.size().eq(0);
 	}
 
 	filter(predicate: Predicate<T>) {
@@ -177,30 +192,22 @@ export class ArrayChain<T extends defined> extends Chain<T[]> {
 		return this.filter((item) => !set.has(item));
 	}
 
-	/**
-	 * Get the item at the given index.
-	 */
+	/** Get the item at the given index. */
 	at(index: number) {
 		return new Chain(this.value[index - 1]);
 	}
 
-	/**
-	 * Get the item at the given index (zero indexed)
-	 */
+	/** Get the item at the given index (zero indexed) */
 	at0(index: number) {
 		return new Chain(this.value[index]);
 	}
 
-	/**
-	 * Get the first item in the array.
-	 */
+	/** Get the first item in the array. */
 	head() {
 		return this.at(0);
 	}
 
-	/**
-	 * Get the last item in the array.
-	 */
+	/** Get the last item in the array. */
 	last() {
 		return this.at(this.value.size() - 1);
 	}
@@ -215,15 +222,18 @@ export class ArrayChain<T extends defined> extends Chain<T[]> {
 		return new ArrayChain(slice);
 	}
 
+	/** Get all but the first item in the array. */
 	tail() {
 		return this.slice(1);
 	}
 
+	/** Get all but the last item in the array. */
 	initial() {
 		return this.slice(0, this.value.size() - 1);
 	}
 
-	take(count: number) {
+	/** Get the first `count` items in the array. */
+	take(count = 1) {
 		return this.slice(0, count);
 	}
 
@@ -233,9 +243,7 @@ export class ArrayChain<T extends defined> extends Chain<T[]> {
 		return new ArrayChain(reversed);
 	}
 
-	/**
-	 * Get the index of the given item.
-	 */
+	/** Get the index of the given item. */
 	indexOf(item: T) {
 		const idx = this.value.indexOf(item) + 1;
 		return new Chain(idx === 0 ? undefined : idx);
@@ -245,30 +253,27 @@ export class ArrayChain<T extends defined> extends Chain<T[]> {
 		return new Chain(this.value.find(predicate));
 	}
 
-	/**
-	 * Check if the array includes the given item.
-	 */
+	findIndex(predicate: Predicate<T>) {
+		const idx = this.value.findIndex(predicate);
+		return new Chain(idx < 0 ? undefined : idx + 1);
+	}
+
+	/** Check if the array includes the given item. */
 	includes(item: T) {
 		return new BoolChain(this.value.includes(item));
 	}
 
-	/**
-	 * Check if every item in the array passes the given predicate.
-	 */
+	/** Check if every item in the array passes the given predicate. */
 	every(predicate: Predicate<T>) {
 		return new BoolChain(this.value.every(predicate));
 	}
 
-	/**
-	 * Check if any item in the array passes the given predicate.
-	 */
+	/** Check if any item in the array passes the given predicate. */
 	some(predicate: Predicate<T>) {
 		return new BoolChain(this.value.some(predicate));
 	}
 
-	/**
-	 * Check if none of the items in the array pass the given predicate.
-	 */
+	/** Check if none of the items in the array pass the given predicate. */
 	none(predicate: Predicate<T>) {
 		return this.every((item, index, array) => !predicate(item, index, array));
 	}
@@ -276,74 +281,74 @@ export class ArrayChain<T extends defined> extends Chain<T[]> {
 	numbers() {
 		return new NumberArrayChain(
 			this.value.map((n) => {
-				const num = tonumber(n);
-				assert(typeIs(num, "number"), `Expected ${n} to be a number`);
-				return num;
+				assert(typeIs(n, "number"), `Expected ${n} to be a number`);
+				return n;
 			}),
 		);
 	}
 
-	// TODO: reduce
-	// TODO: maxBy
-	// TODO: minBy
+	reduce(thru: (acc: T, item: T, index: number, array: readonly T[]) => T) {
+		return new Chain(this.value.reduce(thru));
+	}
+
+	reduceInitial<U extends defined>(thru: (acc: U, item: T, index: number, array: readonly T[]) => U, initial: U) {
+		return new Chain(this.value.reduce(thru, initial));
+	}
+
+	maxBy(score: (item: T) => number) {
+		return new Chain(
+			(this.value.map((item) => [item, score(item)] as const).reduce((a, b) => (a[1] > b[1] ? a : b)) ?? [])[0],
+		);
+	}
+
+	minBy(score: (item: T) => number) {
+		return new Chain(
+			(this.value.map((item) => [item, score(item)] as const).reduce((a, b) => (a[1] < b[1] ? a : b)) ?? [])[0],
+		);
+	}
 }
 
-/**
- * @internal
- */
+/** @internal */
 export class NumberArrayChain extends ArrayChain<number> {
-	/**
-	 * Get the sum of the array.
-	 */
+	/** Get the sum of the array. */
 	sum() {
 		return new NumberChain(this.value.reduce((a, b) => a + b, 0));
 	}
 
-	/**
-	 * Get the smallest number in the array.
-	 */
+	/** Get the smallest number in the array. */
 	min() {
 		return new NumberChain(this.value.reduce((a, b) => math.min(a, b)));
 	}
 
-	/**
-	 * Get the largest number in the array.
-	 */
+	/** Get the largest number in the array. */
 	max() {
 		return new NumberChain(this.value.reduce((a, b) => math.max(a, b)));
 	}
 
-	/**
-	 * Get the average of the array.
-	 */
+	/** Get the average of the array. */
 	mean() {
 		return this.sum().div(this.value.size());
 	}
 }
 
+/** Create an ArrayChain. */
 export function array<T extends defined>(value: T[]) {
 	return new ArrayChain(value);
 }
 
-/**
- * Get children of an instance.
- */
+/** Get children of an instance. */
 export function children(parent: Instance) {
 	return array(parent.GetChildren());
 }
 
 const Players = game.GetService("Players");
 
-/**
- * Get all players in the game.
- */
+/** Get all players in the game. */
 export function players() {
 	return array(Players.GetPlayers());
 }
 
-/**
- * Get all players in the game except the local player.
- */
+/** Get all players in the game except the local player. */
 export function others() {
 	return players().without(Players.LocalPlayer);
 }
